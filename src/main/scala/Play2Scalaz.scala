53385a7fe54da36217c7544bc44a0da15697e646
xuwei-k <6b656e6a69@gmail.com>
2013-12-29 10:57:22 +0900

    InvariantFunctor

--- src/main/scala/Play2Scalaz.scala
+++ src/main/scala/Play2Scalaz.scala
@@ -1,7 +1,8 @@
 import scalaz._
 import scalaz.Isomorphism._
-import play.api.libs.json.{JsResult, JsSuccess, JsError, JsObject, JsArray, JsValue}
+import play.api.libs.json.{JsResult, JsSuccess, JsError, JsObject, JsArray, JsValue, OFormat}
 import play.api.libs.functional.{
+  InvariantFunctor => PlayInvariantFunctor,
   Functor => PlayFunctor,
   Applicative => PlayApplicative,
   Alternative => PlayAlternative,
@@ -24,6 +25,24 @@ package object play2scalaz{
           val zero = ga.identity
         }
     }
+
+  implicit val invariantFunctorIso: TypeclassIso[PlayInvariantFunctor, InvariantFunctor] =
+    new TypeclassIso[PlayInvariantFunctor, InvariantFunctor](
+      new (PlayInvariantFunctor ~~~> InvariantFunctor){
+        def apply[M[_]](m: PlayInvariantFunctor[M]) =
+          new InvariantFunctor[M] {
+            def xmap[A, B](ma: M[A], f: A => B, g: B => A) =
+              m.inmap(ma, f, g)
+          }
+      },
+      new (InvariantFunctor ~~~> PlayInvariantFunctor){
+        def apply[M[_]](m: InvariantFunctor[M]) =
+          new PlayInvariantFunctor[M] {
+            def inmap[A, B](ma: M[A], f: A => B, g: B => A) =
+              m.xmap(ma, f, g)
+          }
+      }
+    )
 
   implicit val functorIso: TypeclassIso[PlayFunctor, Functor] =
     new TypeclassIso[PlayFunctor, Functor](
@@ -151,5 +170,7 @@ package object play2scalaz{
   implicit val jsArrayEqual: Equal[JsArray] =
     Equal.equalA[JsArray]
 
+  implicit val oFormatInvariant: InvariantFunctor[OFormat] =
+    invariantFunctorIso.to(play.api.libs.json.OFormat.invariantFunctorOFormat)
 }
 
