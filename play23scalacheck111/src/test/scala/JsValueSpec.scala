0f8a3d7366c178429e76cf9fe72deaf03a4c9cb7
xuwei-k <6b656e6a69@gmail.com>
2014-05-30 18:33:14 +0900

    remove unused imports

--- play23scalacheck111/src/test/scala/JsValueSpec.scala
+++ play23scalacheck111/src/test/scala/JsValueSpec.scala
@@ -1,11 +1,8 @@
 package play2scalaz
 
-import scalaz._
 import play.api.libs.json._
 import org.scalacheck.{Arbitrary, Gen}
 import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalaCheckBinding._
-import scalaz.std.anyVal._
 import Play2Scalaz._
 
 object JsValueSpec extends scalaz.SpecLite {
