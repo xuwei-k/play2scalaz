9ab175c092def772771692b0ef70562f47b838a5
xuwei-k <6b656e6a69@gmail.com>
2015-06-21 12:28:26 +0900

    scalaprops 0.1.9

--- project/Build.scala
+++ project/Build.scala
@@ -67,7 +67,7 @@ object build extends Build {
     organization := "com.github.xuwei-k",
     licenses := Seq("MIT" -> url("http://opensource.org/licenses/MIT")),
     commands += Command.command("updateReadme")(updateReadme),
-    scalapropsVersion := "0.1.8",
+    scalapropsVersion := "0.1.9",
     pomExtra := (
     <url>https://github.com/xuwei-k/play2scalaz</url>
     <developers>
